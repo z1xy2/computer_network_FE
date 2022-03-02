eba2720d3be022b2abaaacbb4c0109e8d6c199bd
z1xy2 <450484147@qq.com>
2022-03-02 23:01:38 +0800

    群发

--- pages/Chart.vue
+++ pages/Chart.vue
@@ -1,6 +1,7 @@
 <template>
   <el-main>
     <p>Chart</p>
+    <p>用户id:{{ id }}</p>
     <el-input v-model="msg" placeholder="请输入内容"></el-input>
     <el-button type="primary" @click="sendMsg">发送</el-button>
     <ul>
@@ -31,7 +32,7 @@ export default {
     if (!this.id) {
       this.$router.push("/");
     }
-    this.initWebSocket()
+    this.initWebSocket();
     // ws.addEventListener("open", this.handleWsOpen.bind(this), false);
     // ws.addEventListener("close", this.handleWsClose.bind(this), false);
     // ws.addEventListener("error", this.handleWsError.bind(this), false);
@@ -43,41 +44,52 @@ export default {
       if (!msg.trim().length) {
         return;
       }
+      let textmsg = {
+        code: 200,
+        msg: {
+          id: this.id,
+          text: this.msg,
+        },
+      };
+      this.sendWebSocketMsg(textmsg);
       console.log("send msg", this.msg);
     },
-    initWebSocket(){
-        this.websock= new WebSocket("ws://127.0.0.1:8000/chart-channel/");
-        this.websock.onmessage=this.handleWsMessage;
-        this.websock.onopen=this.handleWsOpen;
-        this.websock.onerror=this.handleWsError;
-        this.websock.onclose=this.handleWsClose;
+    initWebSocket() {
+      this.websock = new WebSocket("ws://127.0.0.1:8000/chart-channel/");
+      this.websock.onmessage = this.handleWsMessage;
+      this.websock.onopen = this.handleWsOpen;
+      this.websock.onerror = this.handleWsError;
+      this.websock.onclose = this.handleWsClose;
+        console.log(this)      
     },
-    sendWebSocketMsg(msg){
-        this.websock.send(JSON.stringify(msg))
+    sendWebSocketMsg(msg) {
+      this.websock.send(JSON.stringify(msg));
     },
     handleWsOpen(e) {
-        console.log(1,e)
-        let token=this.$store.getters.isnotUserlogin
-        token=this.id
-        let enterroom={
-            code:100,
-            msg:token
-        }
-        this.sendWebSocketMsg(enterroom)
+      console.log('handleWsOpen调用', e);
+      let enterroom = {
+        code: 100,
+        msg: this.id,
+      };
+      this.sendWebSocketMsg(enterroom);
     },
     handleWsClose(e) {
-        console.log('断开连接',e.code,' ',e.reason, ' ',e.wasClean)
+      console.log("断开连接", e.code, " ", e.reason, " ", e.wasClean);
     },
     handleWsError(e) {
-        console.log(3,e)
+      console.log(3, e);
     },
     handleWsMessage(e) {
-        let message=JSON.parse(e.data)
-        console.log(4,message)
-        if(message.code==100){
-            console.log('进入房间')
-        }
+      let message = JSON.parse(e.data);
+      console.log("handleWsMessage收到了消息", message);
+      if (message.code == 100) {
+        console.log("进入房间");
+      }
     },
+  },
+  beforeDestroy() {
+    console.log("该vc被销毁了");
+    this.websock.close();
   },
 };
 </script>
