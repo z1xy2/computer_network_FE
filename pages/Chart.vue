5c19841f5bd761e4e64b7d8f475243d8c2a13685
z1xy2 <450484147@qq.com>
2022-03-08 10:50:16 +0800

    注释

--- pages/Chart.vue
+++ pages/Chart.vue
@@ -152,18 +152,23 @@ export default {
   name: "Chart",
   data() {
     return {
+      //记录发送的是什么格式
       options: "text",
       msg: "",
       id: "",
+      //信息列表
       msgList: [],
+      //用户列表
       userList: [],
       file: "",
       checkList: new Array(100).fill(false),
       receiveFile: "",
+      //记录接收的文件块
       chunkListL: [],
     };
   },
   mounted() {
+    //挂载函数，提供整个页面，WebSocket的初始化，
     this.id = localStorage.getItem("id");
     console.log(this.id, "entered chart");
     if (!this.id) {
@@ -178,20 +183,25 @@ export default {
   },
   methods: {
     sendVedio() {
+      //请求视频聊天
       var request = {
         code: 600,
       };
       this.sendWebSocketMsg(request);
+      //打开视频链接界面
       window.open("http://127.0.0.1:8081/p2p?type=offer");
     },
     download(text) {
+      //提供URL下载链接
       window.open(text);
     },
     getBigFile(file) {
       this.file = file;
       console.log(file);
     },
+    //选定用户发送消息,可以是文件文字图片等
     ssendMsg() {
+      //如果为文本消息
       if (this.options == "text") {
         const msg = this.msg.trim();
         if (!msg.length) {
@@ -204,6 +214,7 @@ export default {
             id: this.id,
             text: msg,
             time: new Date().toString(),
+            //选中哪些用户
             seleUser: this.seleUser,
           },
         };
@@ -213,6 +224,7 @@ export default {
         //为图片也是发送base64的文本且操作和文字相同所以也是250
         let textmsg = {
           code: 250,
+          //记录消息的一些信息
           msg: {
             id: this.id,
             text: this.image,
@@ -226,7 +238,7 @@ export default {
         //文件发送的虽然也是文本但要另操作
         const blob = this.file.raw;
         console.log(blob);
-        //后面改为40960
+        //文件一块为409600字节
         const bytesPerPiece = 409600;
         const totalSize = blob.size;
         let start = 0;
@@ -241,13 +253,14 @@ export default {
           }
           // 切割文件
           var chunk = blob.slice(start, end);
-          //getvase是异步函数，造成了还未赋值完就执行了后面的操作，导致结果出错，并非chunk作用域的问题,详情见收藏夹!
+          //getvase是异步函数，造成了还未赋值完就执行了后面的操作，导致结果出错，并非chunk作用域的问题,
           //时间回调函数和promise中的then后面的函数都是异步的，写代码需要特别小心
           this.getBase64(chunk).then((res) => {
             console.log(res);
             // 发送数据到服务器
             this.sendWebSocketMsg({
               code: 350,
+              //记录消息的一些信息
               msg: {
                 id: this.id,
                 time: time,
@@ -266,8 +279,10 @@ export default {
         console.log("同步已执行完成");
       }
     },
+    //对所有用户群发消息，可以是文件文字图片等
     sendMsg() {
       //如果为文本
+      //如果msg为空不发送
       if (this.options == "text") {
         const msg = this.msg.trim();
         if (!msg.length) {
@@ -276,6 +291,7 @@ export default {
         //群发code200
         let textmsg = {
           code: 200,
+          //记录消息的一些信息
           msg: {
             id: this.id,
             text: msg,
@@ -287,9 +303,11 @@ export default {
       } else if (this.options == "image") {
         //为图片
         let textmsg = {
+          //请求码为200
           code: 200,
           msg: {
             id: this.id,
+            //图片为base64编码
             text: this.image,
             time: new Date().toString(),
           },
@@ -299,7 +317,7 @@ export default {
       } else if (this.options == "file") {
         const blob = this.file.raw;
         console.log(blob);
-        //后面改为40960
+        //文件一块为409600字节
         const bytesPerPiece = 409600;
         const totalSize = blob.size;
         let start = 0;
@@ -307,6 +325,7 @@ export default {
         var count = 0;
         var time = new Date().toString();
         var fileName = blob.name;
+        //对每一个文件块进行遍历
         while (start < blob.size) {
           end = start + bytesPerPiece;
           if (end > blob.size) {
@@ -318,9 +337,10 @@ export default {
           //时间回调函数和promise中的then后面的函数都是异步的，写代码需要特别小心
           this.getBase64(chunk).then((res) => {
             console.log(res);
-            // 发送数据到服务器
+            // 发送数据到服务器请求码为300
             this.sendWebSocketMsg({
               code: 300,
+              //记录文件块号,发送时间，用户等文件信息
               msg: {
                 id: this.id,
                 time: time,
@@ -330,6 +350,7 @@ export default {
                 fileName: fileName,
               },
             });
+            //块号增加
             count++;
           });
           start = end;
@@ -339,6 +360,7 @@ export default {
       }
     },
     initWebSocket() {
+      //初始化websocket绑定函数，请求连接等等
       this.websock = new WebSocket("ws://127.0.0.1:8000/chart-channel/");
       this.websock.onmessage = this.handleWsMessage;
       this.websock.onopen = this.handleWsOpen;
@@ -346,6 +368,7 @@ export default {
       this.websock.onclose = this.handleWsClose;
       console.log(this);
     },
+    //发送API，将信息转换为json后发送
     sendWebSocketMsg(event) {
       this.websock.send(JSON.stringify(event));
     },
@@ -358,12 +381,15 @@ export default {
       };
       this.sendWebSocketMsg(enterroom);
     },
+    //连接关闭后执行
     handleWsClose(e) {
       console.log("断开连接", e.code, " ", e.reason, " ", e.wasClean);
     },
+    //连接出错执行
     handleWsError(e) {
       console.log("handleWsError收到错误", e);
     },
+    //处理接收到消息的函数
     handleWsMessage(e) {
       console.log("handleWsMessage收到的event", e);
       let response = e.data; //取到回应
@@ -375,32 +401,40 @@ export default {
         console.log("有人进入房间");
         this.userList = response["userList"];
       } else if (response.code == 101) {
+      //回应为101更新用户列表，有人离开房间
         console.log("有人离开房间");
         this.userList = response["userList"];
       } else if (response.code == 200) {
+        //回应为200有人群发消息，添加一条消息
         console.log("有人群发消息");
         this.msgList.push(response["msg"]);
       } else if (response.code == 300) {
+        //回应为300有人群发文件，添加一条消息
         console.log("有人群发文件");
       } else if (response.code == 301) {
         this.msgList.push(response["msg"]);
       } else if (response.code == 600) {
+        //回应为600有人请求视频聊天，为其他用户通知
         this.$alert("用户请求视频聊天", {
           confirmButtonText: "确定",
           callback: (action) => {
             if(action=='confirm'){ 
+              //如果接受则跳转到视频通话页面
               window.open("http://127.0.0.1:8081/p2p?type=answer");
             }            
           },
         });
       }
     },
+    //选中所有用户
     selectAll() {
       this.checkList = new Array(100).fill(true);
     },
+    //清空选中用户
     clearAll() {
       this.checkList = new Array(100).fill(false);
     },
+    //将file类型转换为base64字符串
     getBase64(file) {
       return new Promise(function (resolve, reject) {
         let reader = new FileReader();
@@ -417,7 +451,7 @@ export default {
         };
       });
     },
-
+    //对图片文件进行base64编码
     getImgFile(file) {
       console.log(file);
       this.getBase64(file.raw).then((res) => {
@@ -438,6 +472,7 @@ export default {
     this.websock.close();
   },
   computed: {
+    //实时更新目标接收用户
     seleUser() {
       console.log("计算seleUser");
       var seleUser = [];
